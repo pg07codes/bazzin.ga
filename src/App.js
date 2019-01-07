63799ac884b8722007734d7ce6b5e147ff8e0934
pg07codes <pg07codes@gmail.com>
2019-01-07 21:16:05 +0530

    bootstraping

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component , Fragment} from 'react';
+import Navbar from './components/Navbar'
 
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <p>my app</p>
-      </div>
+        <Fragment>
+            <Navbar/>
+
+        </Fragment>
     );
   }
 }
