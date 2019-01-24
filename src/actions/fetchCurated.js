f5a13bd07c4358c59a083796e6424d6523837ecd
pg07codes <pg07codes@gmail.com>
2019-01-24 22:54:10 +0530

    envs added

--- src/actions/fetchCurated.js
+++ src/actions/fetchCurated.js
@@ -1,12 +1,13 @@
 /*created by Pranav Gupta(pg07codes) on 22/1/19*/
 
 import Unsplash , {toJson}from 'unsplash-js'
-import KEYS from '../config'
-let {UN_API_KEY,UN_API_SECRET}=KEYS
+// import KEYS from '../config'
+// let {UN_API_KEY,UN_API_SECRET}=KEYS
+// THESE ARE COMMENTED SO THAT THE NETLIFY MAY PICK KEYS FROM ENV VARS and 'process.env.' for netlify builds only #JUGAAD
 
 const unsplash = new Unsplash({
-    applicationId: `${UN_API_KEY}`,
-    secret: `${UN_API_SECRET}`,
+    applicationId: process.env.UN_API_KEY,
+    secret: process.env.UN_API_SECRET
 })
 
 
--- src/fbConfig.js
+++ src/fbConfig.js
@@ -1,22 +1,23 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
-import FB_CONFIG from './fbConfigKeys'
-
-let {FB_API_KEY,
-    FB_AUTH_DOMAIN,
-    FB_DATABASE_URL,
-    FB_PROJECT_ID,
-    FB_STORAGE_BUCKET,
-    FB_MESSAGING_SENDER_ID}=FB_CONFIG
+// import FB_CONFIG from './fbConfigKeys'
+//
+// let {FB_API_KEY,
+//     FB_AUTH_DOMAIN,
+//     FB_DATABASE_URL,
+//     FB_PROJECT_ID,
+//     FB_STORAGE_BUCKET,
+//     FB_MESSAGING_SENDER_ID}=FB_CONFIG
+//  THESE ARE COMMENTED SO THAT THE NETLIFY MAY PICK KEYS FROM ENV VARS and 'process.env.' for netlify builds only #JUGAAD
 
 let firebaseConfig= {
-    apiKey: FB_API_KEY,
-    authDomain: FB_AUTH_DOMAIN,
-    databaseURL: FB_DATABASE_URL,
-    projectId: FB_PROJECT_ID,
-    storageBucket: FB_STORAGE_BUCKET,
-    messagingSenderId: FB_MESSAGING_SENDER_ID
+    apiKey: process.env.FB_API_KEY,
+    authDomain: process.env.FB_AUTH_DOMAIN,
+    databaseURL: process.env.FB_DATABASE_URL,
+    projectId: process.env.FB_PROJECT_ID,
+    storageBucket: process.env.FB_STORAGE_BUCKET,
+    messagingSenderId: process.env.FB_MESSAGING_SENDER_ID
 }
 
 firebase.initializeApp(firebaseConfig);
