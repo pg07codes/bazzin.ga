96b7f67f21927c6d159f54feb8d4aaca08dc12ba
pg07codes <pg07codes@gmail.com>
2019-01-27 23:16:24 +0530

    deploy fix #sick

--- src/actions/fetchCurated.js
+++ src/actions/fetchCurated.js
@@ -1,8 +1,8 @@
 /*created by Pranav Gupta(pg07codes) on 22/1/19*/
 
 import Unsplash , {toJson}from 'unsplash-js'
-import KEYS from '../config.json'
-let {UN_API_KEY,UN_API_SECRET}=KEYS
+// import KEYS from '../config.json'
+// let {UN_API_KEY,UN_API_SECRET}=KEYS
 // THESE ARE COMMENTED SO THAT THE NETLIFY MAY PICK KEYS FROM ENV VARS and 'process.env.' for netlify builds only #JUGAAD
 
 // const unsplash = new Unsplash({
--- src/fbConfig.js
+++ src/fbConfig.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
-import FB_CONFIG from './fbConfigKeys'
-
-let {FB_API_KEY,
-    FB_AUTH_DOMAIN,
-    FB_DATABASE_URL,
-    FB_PROJECT_ID,
-    FB_STORAGE_BUCKET,
-    FB_MESSAGING_SENDER_ID}=FB_CONFIG
+// import FB_CONFIG from './fbConfigKeys'
+//
+// let {FB_API_KEY,
+//     FB_AUTH_DOMAIN,
+//     FB_DATABASE_URL,
+//     FB_PROJECT_ID,
+//     FB_STORAGE_BUCKET,
+//     FB_MESSAGING_SENDER_ID}=FB_CONFIG
 //  THESE ARE COMMENTED SO THAT THE NETLIFY MAY PICK KEYS FROM ENV VARS and 'process.env.' for netlify builds only #JUGAAD
 //
 // let firebaseConfig= {
